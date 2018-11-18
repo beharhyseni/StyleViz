39a5ad669c8fe2dc6c8dc9850ce2da834c00faa5
Behar Hyseni <behar.hyseni@alumni.ubc.ca>
2018-11-17 22:37:21 -0800

    project-setup

---
+++ src/main/java/ImageCreator.java
@@ -0,0 +1,20 @@
+package java;
+
+public abstract class ImageCreator {
+
+
+    protected void createBackground(){
+
+    }
+
+    protected void createStem(){
+
+    }
+
+    protected void createFlower(){
+
+    }
+
+
+
+}
