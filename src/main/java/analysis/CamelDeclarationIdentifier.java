6ecaff054c03fe90264a1b7c433b4d360f80a4ff
Daniel Henry Ng <dandan@dhcp-128-189-225-52.ubcsecure.wireless.ubc.ca>
2018-11-21 20:08:52 -0800

    testing commit

--- src/main/java/analysis/CamelDeclarationIdentifier.java
+++ src/main/java/analysis/CamelDeclarationIdentifier.java
@@ -26,6 +26,22 @@ public class CamelDeclarationIdentifier extends DeclarationProcessor {
         // TODO use goodClassNames badClassNames
         // TODO goodClassNames.size / classNames.size x 100 (no percentage) return string
         // TODO update JSON
+        for (String name: classNames) {
+            if (Character.isUpperCase(name.charAt(0))) {
+                // valid class name
+                // check if one word (split on capital letters)
+                // if one word, call DataProvider
+                // if >1 word, call DataProvider on each word
+                int lastCap = 0;
+                for (int i=1; i<name.length()-1; i++) {
+                    if (Character.isUpperCase(name.charAt(i))) {
+                        String subWord = name.substring(lastCap, i);
+                    }
+                }
+            } else {
+                // name is not valid
+            }
+        }
     }
 
     @Override
