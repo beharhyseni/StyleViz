70983f738510303a607c6d7cef01f1af8e0fc5c2
Christine <chris_tine_vu_654@hotmail.com>
2018-11-22 22:49:37 -0800

    initialized core JSON

--- src/main/java/analysis/CamelDeclarationIdentifier.java
+++ src/main/java/analysis/CamelDeclarationIdentifier.java
@@ -1,10 +1,17 @@
 package analysis;
 
+import org.json.simple.JSONObject;
 import preprocess.Declaration;
+
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class CamelDeclarationIdentifier extends DeclarationProcessor {
+
+    public static JSONObject camelJSON = new JSONObject();
+
+    public static int currentClass;
 
     private boolean snakeCase = false;
 
@@ -20,6 +27,12 @@ public class CamelDeclarationIdentifier extends DeclarationProcessor {
 
     public CamelDeclarationIdentifier(){ }
 
+    @Override
+    public void initializeJSON(int classIterator) {
+
+        camelJSON.put("Class"+Integer.toString(classIterator), "");
+        //SnakeDeclarationIdentifier.snakeJSON.put("Class"+Integer.toString(classIterator), "");
+    }
 
     @Override
     public void checkClassName(List<String> classNames) {
@@ -27,21 +40,42 @@ public class CamelDeclarationIdentifier extends DeclarationProcessor {
         // TODO goodClassNames.size / classNames.size x 100 (no percentage) return string
         // TODO update JSON
         for (String name: classNames) {
+            List<String> substringsOfName = new ArrayList<>();
+            boolean validName = true;
+
             if (Character.isUpperCase(name.charAt(0))) {
-                // valid class name
-                // check if one word (split on capital letters)
-                // if one word, call DataProvider
-                // if >1 word, call DataProvider on each word
-                int lastCap = 0;
-                for (int i=1; i<name.length()-1; i++) {
+                int indexOfLastCap = 0;
+                // populate substringsOfName with k-1 substrings of class name with k total substrings
+                for (int i=1; i<name.length(); i++) {
                     if (Character.isUpperCase(name.charAt(i))) {
-                        String subWord = name.substring(lastCap, i);
+                        String subWord = name.substring(indexOfLastCap, i);
+                        substringsOfName.add(subWord);
+                        indexOfLastCap = i;
                     }
+                }
+                // add kth substring to substringsOfName
+                String lastChar = Character.toString(name.charAt(name.length()-1));
+                String lastSubstring = name.substring(indexOfLastCap, name.length()-1).concat(lastChar);
+                substringsOfName.add(lastSubstring);
+
+                // loop through and check each substring
+                for (String substring: substringsOfName) {
+                    if (!DeclarationProcessor.isInDictionary(substring)) {
+                        validName = false;
+                    }
+                }
+                if (validName) {
+                    goodClassNames.add(name);
+                } else {
+                    badClassNames.add(name);
                 }
             } else {
                 // name is not valid
+                badClassNames.add(name);
             }
         }
+        // TODO: update JSON with class names
+
     }
 
     @Override
--- src/main/java/analysis/SnakeDeclarationIdentifier.java
+++ src/main/java/analysis/SnakeDeclarationIdentifier.java
@@ -1,10 +1,15 @@
 package analysis;
 
+import org.json.simple.JSONObject;
 import preprocess.Declaration;
 
 import java.util.List;
 
 public class SnakeDeclarationIdentifier extends DeclarationProcessor {
+
+    public static JSONObject snakeJSON = new JSONObject();
+
+    public static int currentClass;
 
     private List<Declaration> declarations;
     private boolean snakeCase = true;
@@ -20,7 +25,12 @@ public class SnakeDeclarationIdentifier extends DeclarationProcessor {
 
     public SnakeDeclarationIdentifier() { }
 
+    @Override
+    public void initializeJSON(int classIterator) {
 
+        snakeJSON.put("Class"+Integer.toString(classIterator), "");
+        //SnakeDeclarationIdentifier.snakeJSON.put("Class"+Integer.toString(classIterator), "");
+    }
     @Override
     public void checkClassName(List<String> classNames) {
         //TODO
